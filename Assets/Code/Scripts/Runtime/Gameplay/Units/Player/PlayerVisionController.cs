eed85acb3e1887061d9937ce4ad8d505edbfe1ab
JoJaeHyeon <whwogus947@naver.com>
2024-12-08 22:10:07 +0900

    Hill scene prototype completed

--- Assets/Code/Scripts/Runtime/Gameplay/Units/Player/PlayerVisionController.cs
+++ Assets/Code/Scripts/Runtime/Gameplay/Units/Player/PlayerVisionController.cs
@@ -4,6 +4,9 @@ namespace Com2usGameDev
 {
     public class PlayerVisionController : MonoBehaviour
     {
+        public Vector2 offset;
+        public Transform background;
+
         private Camera mainCam;
         private float ySize;
         private float xSize ;
@@ -18,11 +21,12 @@ namespace Com2usGameDev
         void Update()
         {
             var originPos = transform.position;
-            Vector3 playerPos = new(Mathf.FloorToInt(originPos.x / xSize + 0.5f) * xSize, Mathf.FloorToInt(originPos.y / ySize + 0.5f) * ySize, 0)
+            Vector3 playerPos = new(Mathf.FloorToInt(originPos.x / xSize + 0.5f) * xSize + offset.x, Mathf.FloorToInt(originPos.y / ySize + 0.5f) * ySize + offset.y, 0)
             {
                 z = -10
             };
             mainCam.transform.position = playerPos;
+            background.localPosition = playerPos.y * 0.33f * Vector3.up;
         }
     }
 }
