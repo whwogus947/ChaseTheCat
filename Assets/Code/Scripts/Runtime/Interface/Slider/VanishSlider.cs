23f3945f6fc0b7d9ae930567aa44e986d8b1627e
JoJaeHyeon <whwogus947@naver.com>
2024-12-13 15:24:07 +0900

    Vanish slider effect complete
    dissolve,
    shaking,
    vanish

--- Assets/Code/Scripts/Runtime/Interface/Slider/VanishSlider.cs
+++ Assets/Code/Scripts/Runtime/Interface/Slider/VanishSlider.cs
@@ -6,16 +6,21 @@ namespace Com2usGameDev
     public class VanishSlider : MonoBehaviour
     {
         public bool hide;
+        public bool shake;
 
         private Slider main;
         private Slider sub;
         private CountdownTimer timer;
+        private CanvasGroup canvasGroup;
+        private readonly float shakeSize = 0.42f;
+        private const float fadeTime = 3.5f;
 
-        void Start()
+        void Awake()
         {
-            timer = new(1f);
+            timer = new(0f);
             sub = transform.GetChild(0).GetComponent<Slider>();
             main = transform.GetChild(1).GetComponent<Slider>();
+            canvasGroup = GetComponent<CanvasGroup>();
 
             if (hide)
                 SetSliderTransparency(0);
@@ -30,23 +35,31 @@ namespace Com2usGameDev
 
         void Update()
         {
-            if (Mathf.Abs(sub.value - main.value) > 0.02f)
+            bool hasGap = Mathf.Abs(sub.value - main.value) > 0.02f;
+            if (hasGap)
             {
                 sub.value = Mathf.Lerp(sub.value, main.value, Time.deltaTime * 1f);
                 if (sub.value - main.value < 0.05f)
                 {
                     sub.value = main.value;
+                    ShakePosition(0);
                 }
             }
 
             if (!hide)
                 return;
 
-            if (!timer.IsFinished)
+            if (!timer.IsFinished && hasGap)
             {
                 timer.Tick();
-                var leftover = Mathf.Max(0.2f, timer.TimeRemaining) / 0.2f;
+                var leftover = Mathf.Min(0.2f, timer.TimeRemaining) / 0.2f;
                 SetSliderTransparency(leftover);
+                if (shake)
+                {
+                    var tempSize = timer.TimeRemaining / fadeTime;
+                    float shakeStrength = Mathf.Max(0, (tempSize - 0.72f) * shakeSize * tempSize);
+                    ShakePosition(shakeStrength);
+                }
             }
         }
 
@@ -58,12 +71,16 @@ namespace Com2usGameDev
 
         private void InvokeTimer()
         {
-            timer = new(0.5f);
+            timer = new(fadeTime);
+        }
+
+        private void ShakePosition(float size)
+        {
+            transform.localPosition = Random.insideUnitCircle * size;
         }
 
         private void SetSliderTransparency(float alpha)
         {
-            CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
             if (canvasGroup == null)
             {
                 canvasGroup = gameObject.AddComponent<CanvasGroup>();
